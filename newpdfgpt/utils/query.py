4629b9f7b50ba452de4e1700aac8f6e9032d9f88
deepaknagar60 <deepak.nagar@primathon.in>
2024-04-03 13:49:46 +0530

    added prompt for answering

--- newpdfgpt/utils/query.py
+++ newpdfgpt/utils/query.py
@@ -8,9 +8,21 @@ from langchain_pinecone import PineconeVectorStore
 from langchain_community.vectorstores import FAISS
 import faiss
 from dotenv import load_dotenv
+from langchain import PromptTemplate
 
 
+prompt_template = """You are helpful information giving QA System and make sure you don't answer anything 
+not related to following context. You are always provide useful information & details available in the given context. Use the following pieces of context to answer the question at the end. 
+If you don't know the answer, just say that Hmm, I am not sure, don't try to make up an answer. 
 
+{context}
+
+Question: {question}
+Helpful Answer:"""
+
+qa_prompt = PromptTemplate(
+    template=prompt_template, input_variables=["context", "question"]
+)
 
 
 load_dotenv()
@@ -32,9 +44,6 @@ def answer_question(category, namespace , question):
         
         vectorstore = FAISS.load_local(namespace_directory, embeddings,allow_dangerous_deserialization=True)
         
-        # file_path = os.path.join(namespace_directory, 'index.faiss')
-        # index = faiss.read_index(file_path)
-        # print(index)
         
         query_result = vectorstore.similarity_search(query=question, k=3)
         llm = ChatOpenAI(
@@ -47,7 +56,7 @@ def answer_question(category, namespace , question):
             llm=llm,
             chain_type="stuff",
             retriever=vectorstore.as_retriever(),
-             
+            chain_type_kwargs={"prompt": qa_prompt}, 
             # return_source_documents=True
         )
 
